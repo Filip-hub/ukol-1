126e1c1fea42cb8f051f05167a77497af672fd0f
Filip Votek <f.vojtek01@gmail.com>
2020-12-14 17:06:02 +0100

    1.verze

---
+++ ConsoleApp1 14.12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1_14._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte pozdrav");
+            string pozdrav =Console.ReadLine();
+            Console.WriteLine("Zadejte jmeno");
+            string jmeno = Console.ReadLine();
+            Console.WriteLine(pozdrav + jmeno);
+            Console.ReadKey();
+        }
+    }
+}
